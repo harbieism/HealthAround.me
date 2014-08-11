729faa24cbbdc62dbbbe71c4af0bd06b272be740
harbieism <jow598@utulsa.edu>
2014-08-11 12:09:15 -0500

    Created a model for FastFood geopoints.

---
+++ data/models/fast_food.py
@@ -0,0 +1,10 @@
+from django.contrib.gis.db import models
+
+ class FastFood(models.Model):
+    '''Data from ReferenceUSA on the location of fast food restaurants.'''
+
+    class Meta:
+        verbose_name_plural = "fast-food"
+        app_label = "data"
+
+    pnt = models.PointField()
