e485f296bd17047fa84c77b7ab3a8df2a5871c19
harbieism <jow598@utulsa.edu>
2014-08-11 14:33:13 -0500

    Modified the loader and migrated changes

--- data/fast_food/loading.py
+++ data/fast_food/loading.py
@@ -14,12 +14,16 @@ def fast_food_importer():
 def fast_food_db_importer():
     path = 'data/fast_food/fast_food_lat_lon_ok.csv'
     reader = csv.reader(file(path))
-    count = 0
+    count = 1
     for lat, lng in reader:
         float_lat = float(lat)
         float_lng = float(lng)
         new_pnt = Point(float_lng, float_lat)
-        new_fast_food_location = FastFood.objects.get_or_create(pnt=new_pnt)
+        new_fast_food_location, created = FastFood.objects.get_or_create(
+        	pnt=new_pnt,
+        	loader_id=count
+        )
         new_fast_food_location.save()
         count += 1
+        print count
     logger.info("Imported {} fast food locations".format(count))
